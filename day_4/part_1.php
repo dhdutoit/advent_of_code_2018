1bcc0da8a73cedf1794f6fcc8810c50bc9b23ad0
David du Toit <dhdutoit@gmail.com>
2018-12-21 15:20:04 +0200

    Complete part 1 * to my future self I apologise

--- day_4/part_1.php
+++ day_4/part_1.php
@@ -1,7 +1,7 @@
 <?php
 
-// $input = file('input');
-$input = file('test_input');
+$input = file('input');
+// $input = file('test_input');
 
 // build ordered list of data
 $sortedData = [];
@@ -13,53 +13,64 @@ foreach ($input as $timeEvent) {
 ksort($sortedData);
 $sortedData = array_map('trim', $sortedData);
 
-var_dump($sortedData);
-
 // get schedules organised
 $guardSchedules = [];
 foreach ($sortedData as $key => $event) {
 
     if (preg_match("/Guard\s#\d+/", $event)) {
-        if (!key_exists($event, $guardSchedules)) {
-            $currentGuard = $event;
-            $guardSchedules[$currentGuard] = [];
+
+        $currentGuardId = [];
+        preg_match('/\d+/', $event, $currentGuardId);
+        $currentGuardId = $currentGuardId[0];
+
+        if (!key_exists($currentGuardId, $guardSchedules)) {
+            $guardSchedules[$currentGuardId] = [];
         }
     } else {
         $date = ((new DateTime())->setTimestamp($key))->format("Y-m-d H:i");
-        $guardSchedules[$currentGuard][$date] = $event;
+        $guardSchedules[$currentGuardId][$date] = $event;
     }
 
 }
 
 //calculate sleeping for each
+$sleepRecord = [];
 foreach ($guardSchedules as $guard => $schedule) {
+
     $sleep;
     $totalSleep = 0;
+    $minuteRange = [];
+
     foreach ($schedule as $timeStamp => $event) {
         if ("wakes up" === $event) {
-            $period = (new DateTime($sleep))->diff(new DateTime($timeStamp));
+
+            $sleepDate = new DateTime($sleep);
+            $timeStamp = new DateTime($timeStamp);
+
+            $period = ($sleepDate)->diff($timeStamp);
             $totalSleep += $period->format('%i');
+
+            // get minutes and build a range
+            $minuteRange = array_merge($minuteRange, range($sleepDate->format('i'), ($timeStamp->format('i'))-1));
+
         } else {
+            // get the minute he fell asleep on
             $sleep = $timeStamp;
         }
     }
-    echo "--total sleep : " . $totalSleep . "\n";
+
+    $minutesSleptIn = array_count_values($minuteRange);
+    $mostSleptMinute = current(array_keys($minutesSleptIn, max($minutesSleptIn)));
+
+    if ($totalSleep > $sleepRecord[1]) {
+        $sleepRecord = [
+            $guard,
+            $totalSleep,
+            $mostSleptMinute,
+        ];
+    }
 }
 
-print_r($guardSchedules);
-
-
-//next: find guard IDs
-// $guards = preg_grep("/Guard\s#\d+/", $sortedData);
-// print_r($guards);
-// echo array_search("Guard #10 begins shift", $guards);
-
-
-echo "done....." . count($sortedData) . PHP_EOL;
-
-
-//then: find sleep minutes totals
-//then: find most sleping guard
-//then: find most used sleep minute
-
-// var_dump($input);
+//print_r($guardSchedules);
+$answer = $sleepRecord[0] * $sleepRecord[2];
+echo $answer . "\n";
